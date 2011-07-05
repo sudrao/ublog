25fde4c81318580dbad6129802c47bcc2958ba38
Sudhir Rao <sudrao@gmail.com>
2011-07-05 16:35:15 -0700

    Make migration from schema so we can start with rake db:migrate.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090805210857) do
+ActiveRecord::Schema.define(:version => 20110705231052) do
 
   create_table "assets", :force => true do |t|
     t.string   "filename"
