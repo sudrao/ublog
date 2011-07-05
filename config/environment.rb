58a7d39df362387c08a4eb0bec991addd386af36
Sudhir Rao <sudrao@gmail.com>
2011-07-05 09:01:58 -0700

    Get acts_as_solr working.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-SOLR_PATH='/auto/ROR/apps/apache-solr-3.2.0/example'
+SOLR_PATH='/Users/sudhir/apps/apache-solr-3.3.0/example'
 
 # Initialize the rails application
 Ub3::Application.initialize!
--- vendor/plugins/acts_as_solr_reloaded/config/solr_environment.rb
+++ vendor/plugins/acts_as_solr_reloaded/config/solr_environment.rb
@@ -4,24 +4,24 @@ require "uri"
 require "fileutils"
 require "yaml"
 dir = File.dirname(__FILE__)
-SOLR_PATH = File.expand_path("#{dir}/../lib/solr") unless defined? SOLR_PATH
+SOLR_PATH = File.expand_path("#{dir}/../solr") unless defined? SOLR_PATH
 
-#RAILS_ROOT = File.expand_path("#{File.dirname(__FILE__)}/../test") unless defined? RAILS_ROOT
+RAILS_ROOT = File.expand_path("#{File.dirname(__FILE__)}/../test") unless defined? RAILS_ROOT
 unless defined? SOLR_LOGS_PATH
-  SOLR_LOGS_PATH = ENV["SOLR_LOGS_PATH"] || Rails.root.join("log")
+  SOLR_LOGS_PATH = ENV["SOLR_LOGS_PATH"] || "#{RAILS_ROOT}/log"
 end
 unless defined? SOLR_PIDS_PATH
-  SOLR_PIDS_PATH = ENV["SOLR_PIDS_PATH"] || Rails.root.join("tmp", "pids")
+  SOLR_PIDS_PATH = ENV["SOLR_PIDS_PATH"] || "#{RAILS_ROOT}/tmp/pids"
 end
 unless defined? SOLR_DATA_PATH
-  SOLR_DATA_PATH = ENV["SOLR_DATA_PATH"] || Rails.root.join("solr") + Rails.env.to_s
+  SOLR_DATA_PATH = ENV["SOLR_DATA_PATH"] || "#{RAILS_ROOT}/solr/#{ENV['RAILS_ENV']}"
 end
 unless defined? SOLR_CONFIG_PATH
   SOLR_CONFIG_PATH = ENV["SOLR_CONFIG_PATH"] || "#{SOLR_PATH}/solr"
 end
 
 unless defined? SOLR_PORT
-  config = YAML::load_file(Rails.root.join('config', 'solr.yml'))
+  config = YAML::load_file(RAILS_ROOT+'/config/solr.yml')
   raise("No solr environment defined for RAILS_ENV the #{ENV['RAILS_ENV'].inspect}") unless config[ENV['RAILS_ENV']]
   SOLR_PORT = ENV['PORT'] || URI.parse(config[ENV['RAILS_ENV']]['url']).port
 end
