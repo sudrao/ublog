0a4f5aa957b5a447bafafdb73688294ec626c040
Sudhir Rao <sudrao@gmail.com>
2011-07-07 09:52:18 -0700

    Add spec for tags and taglinks.

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -11,6 +11,20 @@ require 'machinist/active_record'
 Home.blueprint do
       ublog_name { "johndoe-#{sn}" }
       name       { "John Doe" }
-      asset_id   { sn }
+      asset_id   { sn.to_i }
       owner      { "someone" }
+end
+
+Blog.blueprint do
+  home_id        { Home.make!.id }
+  content        { "#{sn}: This is a sample message."}
+  is_private     { 0 }
+  thread         { sn.to_i + 1 }
+  prev_in_thread { sn.to_i }
+  proxy          { 'someone' }
+  in_solr        { 0 }
+end
+
+Blog.blueprint(:with_tag) do
+  content { "Hello, my name is #syrah-#{sn}."}
 end
