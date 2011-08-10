d1011bf44518c37b4e485982364a39c06778c6a5
Sudhir Rao <sudrao@gmail.com>
2011-08-10 14:29:13 -0700

    Attempt to get mysql working with travis. Solr is still missing though.

---
+++ spec/travis.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+if ENV['TRAVIS']
+  system "cp spec/.travis.yml config/database.yml"
+  system "mysql -e 'create database ublog_test;' >/dev/null"
+  abort "failed to create mysql database" unless $?.success?
+  system "bundle exec rake db:schema:load"
+end
+
+ENV['DB'] = 'mysql'
+exit 1 unless system "bundle exec rake spec"
+
