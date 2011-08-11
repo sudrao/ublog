e10f525d413a9f267ac186c56743be57a5672c6a
Sudhir Rao <sudrao@gmail.com>
2011-08-10 17:51:27 -0700

    Hide stdout and stderr for solr in travis.

--- spec/travis.rb
+++ spec/travis.rb
@@ -5,7 +5,7 @@ if ENV['TRAVIS']
   system "cp #{path}/.travis-mysql.yml config/database.yml"
   system "mysql -e 'create database ublog_test;' >/dev/null"
   abort "failed to create mysql database" unless $?.success?
-  system "bundle exec rake solr:start >/dev/null"
+  system "bundle exec rake solr:start &>/dev/null"
   system "bundle exec rake db:schema:load"
 end
 
