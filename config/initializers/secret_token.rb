2e7992c8cc455789d491e1783fd42804b10d5148
Sudhir Rao <sudrao@gmail.com>
2011-08-26 14:38:07 -0700

    Blank rails3.1 app. Just kept the .git and other dot-files in root.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ublog::Application.config.secret_token = '407abf3fb8d2ce251e634e9d85be77c78c7217518a0e5fc9b9c9988208b968fc113c572d645e045a397cab466451dd26b1274080e27d8457988bc3eaf095bfbb'
