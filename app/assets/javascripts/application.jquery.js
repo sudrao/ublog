39d997f7493978226cc3c23a9f5de8d92120133a
Sudhir Rao <sudrao@gmail.com>
2011-08-27 12:58:47 -0700

    Get periodic update working with jquery.

--- app/assets/javascripts/application.jquery.js
+++ app/assets/javascripts/application.jquery.js
@@ -1,4 +1,6 @@
 // App specific javascript using JQuery
+
+// Reply links in blogs to pop up a dialog
 $(document).ready(function() {
 	$('.replier').each(function() {
 		var $link = $(this);
@@ -15,5 +17,18 @@ $(document).ready(function() {
 			$dialog.dialog('open');
 			return false;
 		});
+	});
+});
+
+// Periodically update a div on page
+$(document).ready(function() {
+	$('.periodic-replace').each(function() {
+		var $target = $(this);
+		var $msec = parseInt($(this).attr('data-frequency')) * 1000;
+		var $url = $(this).attr('data-url');
+//		var $progress = $(this).attr('data-progress');
+		setInterval(function() {
+			$target.load($url)
+		}, $msec);
 	});
 });
